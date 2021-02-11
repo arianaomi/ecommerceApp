8be813fc26b7702d97e2835b0630c0d871c27522
arianaomi <arianaomi.lp@gmail.com>
2021-02-11 01:33:17 -0600

    add Avatar

--- app/screens/Profile/Profile.js
+++ app/screens/Profile/Profile.js
@@ -1,10 +1,67 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import { Avatar, Icon, Input } from 'react-native-elements'
 
 export default function Profile() {
   return (
     <View>
-      <Text>Perfil</Text>
+      <StatusBar backgroundColor='#128C7E'  />
+      <HeaderBG />
     </View>
   )
 }
+
+function HeaderBG () {
+  return (
+    <View style={styles.avatarInline}>
+      <View style={styles.bg}>
+        <Text style={{color: '#fff', fontSize: 18, fontWeight: 'bold' }}> Nombre </Text>
+        <HeaderAvatar />
+
+
+      </View>
+    </View>
+  )
+}
+
+function HeaderAvatar (props) { 
+  const changePhoto = () => console.log('hi')
+  return (
+    <View>
+      <Avatar
+        source={require('../../../assets/avatar.jpg')}
+        styles={styles.avatar}
+        size='large'  
+        rounded
+        showAccessory
+        onAccessoryPress={changePhoto}
+    
+      >
+         <Avatar.Accessory onPress={changePhoto} style={{width:30, height:30}}/>
+      </Avatar>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  bg:{
+    width: '100%',
+    height: 200,
+    borderBottomRightRadius: 400,
+    borderBottomLeftRadius: 400,
+    backgroundColor: '#128C7E',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop:50
+  },
+  avatarInline:{
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    marginTop: -70
+  },
+  avatar: {
+    width: 80,
+    height: 80
+
+  }
+})
